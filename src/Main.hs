deff9592c5ddcdd5aefd8508cff3f2da73e9d05e
Ryan Mulcahy <rmulcahy@gravie.com>
2017-12-01 15:55:55 -0600

    Problem 1, part 2

--- src/Main.hs
+++ src/Main.hs
@@ -5,4 +5,6 @@ import qualified Problem01 as P01
 main :: IO ()
 main = do
   putStrLn "Solutions: "
-  putStrLn P01.answer
+  putStrLn "Problem 1:"
+  putStrLn $ "Part 1: " ++ P01.partOneAnswer
+  putStrLn $ "Part 2: " ++ P01.partTwoAnswer
