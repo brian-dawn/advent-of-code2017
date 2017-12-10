e070d12574a7f0b3e9dc441e3a2debadd771abba
Carl D. Benson <carl.d.benson@gmail.com>
2017-12-10 13:48:08 -0600

    Did day 10 in a less clever way that required more work up front.

--- src/day10/main.py
+++ src/day10/main.py
@@ -4,49 +4,49 @@ def length_list(file_name):
         return map(int, fp.read().split(','))
 
 
-class Node:
-    def __init__(self, val, prev):
-        self.val = val
-        self.prev = prev
-        self.next = None
-        self.is_head = False
-
-    def flip(self):
-        self.prev, self.next = self.next, self.prev
-
-    def nth(self, n):
-        return self.next.nth(n - 1) if n else self
-
-    def to_str(self, length):
-        return str(self.val) + ((', ' + self.next.to_str(length - 1)) if length - 1 else '')
+def ascii_length_list(file_name):
+    with open('../../resources/' + file_name, 'r') as fp:
+        return map(ord, fp.read())
 
 
-def looped_list(length):
-    head = Node(0, None)
-    head.is_head = True
-    cur = head
-    for i in range(1, length):
-        new = Node(i, cur)
-        cur.next = new
-        cur = new
-    cur.next = head
-    head.prev = cur
-    return head
+def flip_sublist(lst, start_idx, length):
+    def flip_sublist_simple():
+        return lst[:start_idx] + lst[start_idx:start_idx + length][::-1] + lst[start_idx + length:]
+
+    def flip_sublist_overflow():
+        end = length - len(lst) + start_idx
+        sub_list = (lst[start_idx:] + lst[:end])[::-1]
+        back, front = sub_list[:len(lst) - start_idx], sub_list[len(lst) - start_idx:]
+        return front + lst[end:start_idx] + back
+
+    return flip_sublist_simple() if start_idx + length <= len(lst) else flip_sublist_overflow()
 
 
-def flip_section(start_node, length):
-
-    end = start_node.nth(length - 1)
-    print start_node.val, end.val
+def run_instructions(lengths, size, skip=0, idx=0, lst=None):
+    if not lst:
+        lst = [i for i in range(size)]
+    for length in lengths:
+        lst = flip_sublist(lst, idx, length)
+        idx = (idx + length + skip) % size
+        skip += 1
+    return lst
 
 
 def run():
     lengths = length_list('day10.txt')
-    head = looped_list(256)
-    cur = head
-    flip_section(cur, 3)
-    for length in lengths:
-        pass
+    tied = run_instructions(lengths, 256)
+    return tied[0] * tied[1]
+
+
+def run_2():
+    lengths = ascii_length_list('day10.txt')
+    size = len(lengths)
+    idx, lst = 0, None
+    for i in range(64):
+        lst = run_instructions(lengths, 256, skip=size*i, idx=idx, lst=lst)
+    # TODO: xor blocks of 16, convert to hex
+
 
 if __name__ == '__main__':
     print run()
+    print run_2()
