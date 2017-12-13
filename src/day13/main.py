7337fb18e9c5b16eeb88a6850d97890d8a583884
Carl D. Benson <carl.d.benson@gmail.com>
2017-12-13 01:19:52 -0600

    Day 13. Brute forced part two a bit. Wonder if there's a better way.

---
+++ src/day13/main.py
@@ -0,0 +1,26 @@
+import itertools
+
+
+def read_security(file_name):
+    with open('../../resources/' + file_name, 'r') as fp:
+        for line in fp:
+            yield map(int, line.split(': '))
+
+
+def sum_with_offset(offset, firewall):
+    return sum(depth * rng for depth, rng in firewall if (depth + offset) % (rng * 2 - 2) == 0)
+
+
+def run():
+    return sum_with_offset(0, read_security('day13.txt'))
+
+
+def run_2():
+    firewall = [s for s in read_security('day13.txt')]
+    for i in itertools.count():
+        if i % (firewall[0][1] * 2 - 2) and sum_with_offset(i, firewall) == 0:
+            return i
+
+if __name__ == '__main__':
+    print run()
+    print run_2()
