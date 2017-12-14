3e7812bcc717d4372b4bd8e1d482f55ec353d5bb
Carl D. Benson <carl.d.benson@gmail.com>
2017-12-13 23:56:20 -0600

    Clean up some from day 14.

--- src/day14/main.py
+++ src/day14/main.py
@@ -4,34 +4,31 @@ INPUT = 'jxqlasbh'
 
 
 def scrub_region(matrix, row, column):
+    if row in [-1, len(matrix)]\
+            or column in [-1, len(matrix[0])]\
+            or matrix[row][column] != '1':
+        return
     matrix[row][column] = '0'
-    if row != 0 and matrix[row-1][column] == '1':
-        scrub_region(matrix, row - 1, column)
-    if row != len(matrix) - 1 and matrix[row+1][column] == '1':
-        scrub_region(matrix, row + 1, column)
-    if column != 0 and matrix[row][column-1] == '1':
-        scrub_region(matrix, row, column - 1)
-    if column != len(matrix[0]) - 1 and matrix[row][column+1] == '1':
-        scrub_region(matrix, row, column + 1)
+    scrub_region(matrix, row - 1, column)
+    scrub_region(matrix, row + 1, column)
+    scrub_region(matrix, row, column - 1)
+    scrub_region(matrix, row, column + 1)
 
 
 def run():
-    acc = 0
-    for i in range(128):
-        binary = KnotHash.hash_value(INPUT + '-' + str(i), binary=True)
-        acc += len([dig for dig in binary if dig == '1'])
-    return acc
+    return sum(
+        [len([dig for dig in
+              KnotHash.hash_value(INPUT + '-' + str(i), binary=True) if dig == '1'])
+         for i in range(128)])
 
 
 def run_2():
-    binaries = []
+    binaries = map(list, [KnotHash.hash_value(INPUT + '-' + str(i), binary=True) for i in range(128)])
     acc = 0
-    for i in range(128):
-        val = KnotHash.hash_value(INPUT + '-' + str(i), binary=True)
-        binaries.append([dig for dig in val])
     for row, binary in enumerate(binaries):
         for column, char in enumerate(binary):
             if char == '1':
+                # mutating while iterating over. Gross.
                 scrub_region(binaries, row, column)
                 acc += 1
     return acc
