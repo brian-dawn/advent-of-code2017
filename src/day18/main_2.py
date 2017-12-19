bbceb1d3d9eddf807474817f5317e599b3855c7c
Carl D. Benson <carl.d.benson@gmail.com>
2017-12-18 21:20:54 -0600

    Finally got day 18 part 2.

--- src/day18/main_2.py
+++ src/day18/main_2.py
@@ -1,20 +1,26 @@
-from collections import defaultdict
-from collections import deque
-
-
-class State:
-    def __init__(self, prog0, prog1):
-        self.program0 = prog0
-        self.program1 = prog1
+from collections import defaultdict, deque
 
 
 class Program:
     def __init__(self, program_id):
+        self.program_id = program_id
         self._registers = defaultdict(lambda: 0)
         self._registers['p'] = program_id
-        self._send = 0
         self.index = 0
-        self.received = deque()
+        self.waiting = False
+        self.queue = deque()
+        self.send_count = 0
+
+    def send(self, val):
+        self.send_count += 1
+        self.queue.append(val)
+
+    def receive(self, reg_x):
+        if len(self.queue):
+            self[reg_x] = self.queue.popleft()
+            return True
+        self.waiting = True
+        return False
 
     def __getitem__(self, key):
         if isinstance(key, int):
@@ -25,9 +31,9 @@ class Program:
         self._registers[key] = value
 
 
-def _snd(val, state):
-    state.sound = state[val]
-    _jgz(1, 1, state)
+def _snd(val, running, sleeping):
+    sleeping.send(running[val])
+    _jgz(1, 1, running)
 
 
 def _set(reg_x, val_y, state):
@@ -50,9 +56,9 @@ def _mod(reg_x, val_y, state):
     _jgz(1, 1, state)
 
 
-def _rcv(val_x, state):
-    state.recovered = state.sound if state[val_x] else None
-    _jgz(1, 1, state)
+def _rcv(reg_x, running):
+    if running.receive(reg_x):
+        _jgz(1, 1, running)
 
 
 def _jgz(val_x, val_y, state):
@@ -71,19 +77,19 @@ def parse(inst_str):
     instr = parts[0]
     params = map(_maybe_reg_or_val, parts[1:])
     if instr == 'snd':
-        return lambda state: _snd(params[0], state)
+        return lambda running, sleeping: _snd(params[0], running, sleeping)
     if instr == 'set':
-        return lambda state: _set(params[0], params[1], state)
+        return lambda running, _: _set(params[0], params[1], running)
     if instr == 'add':
-        return lambda state: _add(params[0], params[1], state)
+        return lambda running, _: _add(params[0], params[1], running)
     if instr == 'mul':
-        return lambda state: _mul(params[0], params[1], state)
+        return lambda running, _: _mul(params[0], params[1], running)
     if instr == 'mod':
-        return lambda state: _mod(params[0], params[1], state)
+        return lambda running, _: _mod(params[0], params[1], running)
     if instr == 'rcv':
-        return lambda state: _rcv(params[0], state)
+        return lambda running, _: _rcv(params[0], running)
     if instr == 'jgz':
-        return lambda state: _jgz(params[0], params[1], state)
+        return lambda running, _: _jgz(params[0], params[1], running)
 
 
 def read_instructions(file_name):
@@ -91,10 +97,20 @@ def read_instructions(file_name):
         return map(parse, fp.readlines())
 
 
+def still_running(prog, instr):
+    return prog.index < len(instr) and not prog.waiting
+
+
 def run():
     instructions = read_instructions('day18.txt')
-    state = State(Program(0), Program(1))
-
+    running = Program(0)
+    sleeping = Program(1)
+    while still_running(running, instructions) \
+            or still_running(sleeping, instructions):
+        instr = instructions[running.index]
+        instr(running, sleeping)
+        running, sleeping = sleeping, running
+    return [p for p in (running, sleeping) if p.program_id == 0][0].send_count
 
 if __name__ == '__main__':
     print run()
