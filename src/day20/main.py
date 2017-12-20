afd60ab93db79a59bee300812a51934d2f310beb
Carl D. Benson <carl.d.benson@gmail.com>
2017-12-20 11:31:35 -0600

    Started rewriting day 20 to find a general function for the current position.

--- src/day20/main.py
+++ src/day20/main.py
@@ -5,6 +5,7 @@ import operator
 
 Coord = namedtuple('Coord', ['x', 'y', 'z'])
 Particle = namedtuple('Particle', ['num', 'p', 'v', 'a'])
+Func_Particle = namedtuple('Func_Particle', ['num', 'func'])
 
 PATTERN = re.compile(r'p=<(.*),(.*),(.*)>, v=<(.*),(.*),(.*)>, a=<(.*),(.*),(.*)>')
 
@@ -18,9 +19,30 @@ def parse(i_line):
                     a=Coord(*(int(m.group(i)) for i in range(7, 10))))
 
 
-def read_particles(file_name):
+def to_func(coeffs):
+    p, v, a = coeffs
+    return lambda t: p + (v + 0.5 * a) * t + 0.5 * a * t**2
+
+
+def coords_to_func(p_initial, v_initial, a_initial):
+    return lambda t: (f(t) for f in map(to_func, zip(p_initial, v_initial, a_initial)))
+
+
+def parse_func(i_line):
+    i, line = i_line
+    m = PATTERN.search(line)
+    return Func_Particle(num=int(i),
+                         func=coords_to_func(
+                             Coord(*(int(m.group(i)) for i in range(1, 4))),
+                             Coord(*(int(m.group(i)) for i in range(4, 7))),
+                             Coord(*(int(m.group(i)) for i in range(7, 10)))
+                         ))
+
+
+def read_particles(file_name, func=False):
+    mapper = parse_func if func else parse
     with open('../../resources/' + file_name, 'r') as fp:
-        return map(parse, enumerate(fp.readlines()))
+        return map(mapper, enumerate(fp.readlines()))
 
 
 def run():
